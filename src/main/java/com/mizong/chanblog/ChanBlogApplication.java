e6583d245f977130212fae50b2ac5b227664ffb6
李锐 <mizong@2dfire.com>
2019-09-21 15:48:24 +0800

    frist commit

---
+++ src/main/java/com/mizong/chanblog/ChanBlogApplication.java
@@ -0,0 +1,13 @@
+package com.mizong.chanblog;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ChanBlogApplication {
+    public static void main(String[]args){
+        SpringApplication.run(ChanBlogApplication.class,args);
+    }
+}
